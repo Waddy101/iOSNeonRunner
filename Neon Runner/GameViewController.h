26a406a2c48f5758f755e22357cb3430683c08ef
Darren Vong <d.vongy@gmail.com>
2016-11-15 17:26:34 +0000

    Added and linked controller classes to those made on the storyboard

---
+++ Neon Runner/GameViewController.h
@@ -0,0 +1,13 @@
+//
+//  GameViewController.h
+//  Neon Runner
+//
+//  Created by aca13kcv on 15/11/2016.
+//  Copyright © 2016 aca13amw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
