be9f6a8eeed009569d1804615a1867d7965e6cf2
Darren Vong <d.vongy@gmail.com>
2017-01-18 17:37:43 +0000

    Game pauses when sent to background and restarts when re-opened

--- Neon Runner/NRAppDelegate.m
+++ Neon Runner/NRAppDelegate.m
@@ -43,6 +43,12 @@
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    UINavigationController* navController = (UINavigationController*)self.window.rootViewController;
+    if ([navController.topViewController isKindOfClass:[NRGameViewController class]]) {
+        SKView* gameView = (SKView*)navController.topViewController.view;
+        gameView.paused = NO;
+    }
+    NSLog(@"%@", navController.topViewController);
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
